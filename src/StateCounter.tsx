03fe09deebf8a4a01b4045a9c938d4ecf090b752
1m4nim <fancustard9@gmail.com>
2025-05-09 17:04:17 +0900

    3-3-6 子コンポーネントから親コンポーネントへの情報伝達

--- src/StateCounter.tsx
+++ src/StateCounter.tsx
@@ -1,12 +1,29 @@
+// import "./StateCounter.css";
+
+// type Props = {
+//   step: string;
+//   onUpdate: string;
+// };
+
+// export default function StateCounter({ step, onUpdate }: Props) {
+//   const handleClick = () => onUpdate(step);
+//   return (
+//     <button className="cnt" onClick={handleClick}>
+//       <span>{step}</span>
+//     </button>
+//   );
+// }
+
 import "./StateCounter.css";
 
 type Props = {
-  step: string;
-  onUpdate: string;
+  step: number;
+  onUpdate: (step: number) => void;
 };
 
 export default function StateCounter({ step, onUpdate }: Props) {
   const handleClick = () => onUpdate(step);
+
   return (
     <button className="cnt" onClick={handleClick}>
       <span>{step}</span>
