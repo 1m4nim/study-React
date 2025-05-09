91ce2dd77563e1e16ab1cfc4c9894e8a8d3395a8
1m4nim <fancustard9@gmail.com>
2025-05-09 13:04:24 +0900

    3-3-4プロパティ側の検証（PropTypes）

--- src/MyHello.tsx
+++ src/MyHello.tsx
@@ -1,11 +1,35 @@
-type MyHelloProps = {
+// type MyHelloProps = {
+//   myName: string;
+// };
+
+// // export default function MyHello(props: MyHelloProps) {
+// //   return <div>Hello, {props.myName}</div>;
+// // }
+
+// export default function MyHello({ myName }: MyHelloProps) {
+//   return <div>Hello, {myName}</div>;
+// }
+
+//間違った例
+
+// import PropTypes fromn "prop-types";
+
+// function MyHello(props){
+//   return(
+//     <div>Hello,{props.myName}</div>
+//   );
+// }
+
+// MyHello.propTypes={
+//   myName:PropTypes.string.isRequired
+// };
+
+// export default MyHello;
+
+type Props = {
   myName: string;
 };
 
-// export default function MyHello(props: MyHelloProps) {
-//   return <div>Hello, {props.myName}</div>;
-// }
-
-export default function MyHello({ myName }: MyHelloProps) {
-  return <div>Hello, {myName}</div>;
+export default function MyHello({ myName }: Props) {
+  return <div>Hello,{myName}</div>;
 }
--- src/main.tsx
+++ src/main.tsx
@@ -19,24 +19,25 @@ import "./index.css";
 //import TitledPanel from "./TitledPanel";
 import reportWebVitals from "./reportWebVitals";
 //import book from "./book";
-import ListTemplate from "./ListTemplate";
+//import ListTemplate from "./ListTemplate";
+import MyHello from "./MyHello";
 
 //const container = document.getElementById("root");
 
-const books = [
-  {
-    isbn: "978-4-7981-5112-0",
-    title: "独習JavaScript 新版",
-    price: 3080,
-    summary: "JavaScriptの基礎から応用まで解説した入門書。",
-  },
-  {
-    isbn: "978-4-7981-5382-7",
-    title: "React開発入門",
-    price: 3300,
-    summary: "Reactの基本と実践的なコンポーネント設計を学べる。",
-  },
-];
+// const books = [
+//   {
+//     isbn: "978-4-7981-5112-0",
+//     title: "独習JavaScript 新版",
+//     price: 3080,
+//     summary: "JavaScriptの基礎から応用まで解説した入門書。",
+//   },
+//   {
+//     isbn: "978-4-7981-5382-7",
+//     title: "React開発入門",
+//     price: 3300,
+//     summary: "Reactの基本と実践的なコンポーネント設計を学べる。",
+//   },
+// ];
 
 const container: HTMLElement | null = document.getElementById("root");
 
@@ -67,22 +68,24 @@ if (container) {
   //   </TitledPanel>
   // );
 
-  root.render(
-    <ListTemplate src={books}>
-      {(item) => (
-        <>
-          <dt>
-            <a
-              href={`https://wings.msn.to/books/${item.isbn}/${item.isbn}.jpg`}
-            >
-              {item.title}({item.price}円)
-            </a>
-          </dt>
-          <dd>{item.summary}</dd>
-        </>
-      )}
-    </ListTemplate>
-  );
+  // root.render(
+  //   <ListTemplate src={books}>
+  //     {(item) => (
+  //       <>
+  //         <dt>
+  //           <a
+  //             href={`https://wings.msn.to/books/${item.isbn}/${item.isbn}.jpg`}
+  //           >
+  //             {item.title}({item.price}円)
+  //           </a>
+  //         </dt>
+  //         <dd>{item.summary}</dd>
+  //       </>
+  //     )}
+  //   </ListTemplate>
+  // );
+
+  root.render(<MyHello myName="Taro" />);
 } else {
   console.log("Root container not found");
 }
